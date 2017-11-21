6e3a33f166e59e1462e7788a34aa13e28e96c53d
elveskevtar <kevtar@gmail.com>
2017-11-21 14:41:10 -0500

    fixes for certification and add logo

--- showdown.py
+++ showdown.py
@@ -34,12 +34,10 @@ def launched():
 					'if this is your first time')
 
 @ask.intent('TutorialIntent')
-@sup.guide
 def tutorial():
 	with open('tutorial', 'r') as file:
 		data = file.read().replace('\n', ' ')
-	sup.move_to_step('main_menu')
-	return question(data)
+	return statement(data)
 
 @ask.intent('StartGameIntent')
 @sup.guide
@@ -104,49 +102,60 @@ def upgrade(upgrade_type):
 	player = session.attributes['player_turn']
 	cookies = session.attributes['cookie_count'][player]
 	if session.attributes['has_upgrade'][player]:
-		return question('You have already upgraded this turn')
+		return question('You have already upgraded this turn. Try performing an action '
+						'or ending your turn.')
 	if upgrade_type in const.ITEM_STRINGS:
 		item_level = session.attributes['item_level'][player]
 		if item_level == const.ITEMS[-1][0]:
-			return question('You have maxed out item upgrades')
+			return question('You have maxed out item upgrades. Try performing an action '
+							'or ending your turn.')
 		if cookies < const.ITEMS[item_level][2]:
 			return question('You need ' + str(const.ITEMS[item_level][2]) + ' cookies '
-							'to upgrade to ' + const.ITEMS[item_level][1])
+							'to upgrade to ' + const.ITEMS[item_level][1] + '. Try performing '
+							'an action or ending your turn.')
 		session.attributes['item_level'][player] += 1
 		session.attributes['cookie_count'][player] -= const.ITEMS[item_level][2]
 		session.attributes['has_upgrade'][player] = True
-		return question('Successfully upgraded item to ' + const.ITEMS[item_level][1])
+		return question('Successfully upgraded item to ' + const.ITEMS[item_level][1] +
+						'. Try performing an action or ending your turn.')
 	elif upgrade_type in const.STORE_STRINGS:
 		store_level = session.attributes['store_level'][player]
 		if store_level == const.STORES[-1][0]:
-			return question('You have maxed out store upgrades')
+			return question('You have maxed out store upgrades. Try performing an action '
+							'or ending your turn.')
 		if cookies < const.STORES[store_level][2]:
 			return question('You need ' + str(const.STORES[store_level][2]) + ' cookies '
-							'to upgrade to ' + const.STORES[store_level][1])
+							'to upgrade to ' + const.STORES[store_level][1] + '. Try performing '
+							'an action or ending your turn.')
 		session.attributes['store_level'][player] += 1
 		session.attributes['cookie_count'][player] -= const.STORES[store_level][2]
 		session.attributes['has_upgrade'][player] = True
-		return question('Successfully upgraded store to ' + const.STORES[store_level][1])
+		return question('Successfully upgraded store to ' + const.STORES[store_level][1] +
+						'. Try performing an action or ending your turn.')
 	elif upgrade_type in const.AD_STRINGS:
 		ad_level = session.attributes['ad_level'][player]
 		if ad_level == const.ADVERTISING[-1][0]:
-			return question('You have maxed out advertising upgrades')
+			return question('You have maxed out advertising upgrades. Try performing an action '
+							'or ending your turn.')
 		if cookies < const.ADVERTISING[ad_level][2]:
 			return question('You need ' + str(const.ADVERTISING[ad_level][2]) + ' cookies '
-							'to upgrade to ' + const.ADVERTISING[ad_level][1])
+							'to upgrade to ' + const.ADVERTISING[ad_level][1] + '. Try '
+							'performing an upgrade or ending your turn.')
 		session.attributes['ad_level'][player] += 1
 		session.attributes['cookie_count'][player] -= const.ADVERTISING[ad_level][2]
 		session.attributes['has_upgrade'][player] = True
 		return question('Successfully upgraded advertising to ' +
-						const.ADVERTISING[ad_level][1])
-	return question('Could not recognize upgrade option')
+						const.ADVERTISING[ad_level][1] + '. Try performing an action or '
+						'ending your turn.')
+	return question('Could not recognize upgrade option. Try again.')
 
 @ask.intent('ActionIntent', convert={'action_type': str, 'sabotage_target': str})
 @sup.guide
 def action(action_type, sabotage_target):
 	player = session.attributes['player_turn']
 	if session.attributes['has_done'][player]:
-		return question('You have already performed an action this turn')
+		return question('You have already performed an action this turn. Try upgrading or '
+						'ending your turn.')
 	if action_type in const.AD_STRINGS:
 		ad_mult = session.attributes['ad_mult']
 		ad_level = session.attributes['ad_level'][player]
@@ -154,49 +163,56 @@ def action(action_type, sabotage_target):
 		mult = round(uniform(advertising[3], advertising[4]), 1)
 		ad_mult[player] = round(ad_mult[player] + mult, 1)
 		session.attributes['has_done'][player] = True
-		return question(advertising[1] + ' added to your multiplier by ' + str(mult))
+		return question(advertising[1] + ' added to your multiplier by ' + str(mult) +
+						'. Try upgrading or ending your turn.')
 	if action_type in const.SABOTAGE_STRINGS:
 		if sabotage_target not in session.attributes['player_names']:
-			return question('Could not recognize sabotage target')
+			return question('Could not recognize sabotage target. Try again.')
 		target = session.attributes['player_names'].index(sabotage_target)
 		if player == target:
-			return question('Cannot sabotage yourself')
+			return question('Cannot sabotage yourself. Try again.')
 		sabotage_level = session.attributes['sabotage_level'][player]
 		sabotage = const.SABOTAGE[sabotage_level - 1]
 		if sabotage[1] == 'spread rumors':
 			session.attributes['lose_turn'][target] = True
 			session.attributes['has_done'][player] = True
 			return question('Rumors about ' + session.attributes['player_names'][target] +
-							'\'s shop have spread. They lose their cookies this turn.')
+							'\'s shop have spread. They lose their cookies this turn. '
+							'Try upgrading or ending your turn.')
 		elif sabotage[1] == 'anti-advertising':
 			ad_mult = session.attributes['ad_mult']
 			ad_mult[target] = round(ad_mult[target] - 0.5, 1)
 			session.attributes['has_done'][player] = True
-			return question(session.attributes['player_names'][player] + ' ads generated '
-							' negative publicity for ' +
-							session.attributes['player_names'][target] + 'shop')
+			return question(session.attributes['player_names'][player] + '\'s ads generated '
+							'negative publicity for ' +
+							session.attributes['player_names'][target] + '\'s shop. '
+							'Try upgrading or ending your turn.')
 		elif sabotage[1] == 'hire spy':
 			if session.attributes['store_level'][target] == 1:
-				return question('Cannot sabotage. Target\'s store too low')
+				return question('Cannot sabotage. Target\'s store too low. Try advertising, '
+								'upgrading, or ending your turn.')
 			session.attributes['store_level'][target] -= 1
 			session.attributes['has_done'][player] = True
 			return question(session.attributes['player_names'][player] + ' hired a spy '
 							'that infiltrated ' + session.attributes['player_names'][target] +
 							'\'s shop and made terrible marketing decisions. ' +
 							session.attributes['player_names'][target] +
-							' is forced into a store downgrade.')
+							' is forced into a store downgrade. Try upgrading or '
+							'ending your turn.')
 		elif sabotage[1] == 'steal items':
 			if session.attributes['item_level'][target] == 1:
-				return question('Cannot sabotage. Target\'s items too low')
+				return question('Cannot sabotage. Target\'s items too low. Try advertising, '
+								'upgrading, or ending your turn.')
 			session.attributes['item_level'][target] -= 1
 			session.attributes['has_done'][player] = True
 			return question(session.attributes['player_names'][player] + ', against better '
 							'judgement and good business practice, breaks into ' +
 							session.attributes['player_names'][target] + '\'s shop and steals '
 							'everything. ' + session.attributes['player_names'][target] +
-							' is forced into an item downgrade.')
-		return question('Could not recognize sabotage type')
-	return question('Could not recognize action type')
+							' is forced into an item downgrade. Try upgrading or '
+							'ending your turn.')
+		return question('Could not recognize sabotage type. Try again.')
+	return question('Could not recognize action type. Try again.')
 
 @ask.intent('GetStatIntent', convert={'stat': str})
 @sup.guide
@@ -218,9 +234,9 @@ def get_stat(stat):
 	elif stat in const.AD_STRINGS:
 		ad_level = session.attributes['ad_level'][player]
 		ad = const.ADVERTISING[ad_level - 1]
-		return question('You current advertisment is ' + ad[1] + ' with a multiplier range '
+		return question('Your current advertisment is ' + ad[1] + ' with a multiplier range '
 						'from ' + str(ad[3]) + ' to ' + str(ad[4]))
-	return question('Could not recognize what you are asking for')
+	return question('Could not recognize what value you are asking for. Try again.')
 
 @ask.intent('GetUpgradeCostIntent', convert={'upgrade_type': str})
 @sup.guide
@@ -238,7 +254,7 @@ def get_upgrade_cost(upgrade_type):
 		ad_level = session.attributes['ad_level'][player]
 		ad = const.ADVERTISING[ad_level]
 		return question('It costs ' + str(ad[2]) + ' cookies to upgrade to ' + ad[1])
-	return question('Could not recognize what you are asking for')
+	return question('Could not recognize what value you are asking for. Try again.')
 
 @ask.intent('EndTurnIntent')
 @sup.guide
